fd0bbf76f5c28acb7d9037ec354fa58f7b992fa2
Osama Oransa <osa_ora@yahoo.com>
2022-03-17 11:56:55 +0200

    Update init-serverless.sh

--- configs/init-serverless.sh
+++ configs/init-serverless.sh
@@ -13,9 +13,8 @@ echo "Service 'Useraccounts-db' deployed successfully as ephemeral"
 echo "Login to user-accounts-db mysql pod and install the schema using:"
 echo "mysql -u root"
 echo "connect accounts"
-echo "CREATE TABLE `accounts`.`account` (`id` INT NOT NULL AUTO_INCREMENT, `login` VARCHAR(45) NULL, `password` VARCHAR(45) NULL, `email` VARCHAR(45) NULL, PRIMARY KEY (`id`));"
-echo "INSERT INTO `accounts`.`account` (`id`, `login`, `password`, `email`) VALUES ('1', 'Osama', '123', 'osa.ora@acme.com');"
-echo "INSERT INTO `accounts`.`account` (`id`, `login`, `password`, `email`) VALUES ('2', 'Sameh', '123', 'sa.or@acme.com');"
+curl https://raw.githubusercontent.com/osa-ora/UserAccountService/master/scripts/initial_schema.sql
+
 oc apply -f https://raw.githubusercontent.com/osa-ora/UserAccountService/master/configs/image.yaml
 oc apply -f https://raw.githubusercontent.com/osa-ora/UserAccountService/master/configs/build-config.yaml
 oc apply -f https://raw.githubusercontent.com/osa-ora/UserAccountService/master/configs/useraccount-serverless.yaml
